4c6229d21a992f56d77a70022c36dd2a7fbd319f
luantranthannh <luan.tran25@student.passerellesnumeriques.org>
2024-05-21 16:45:17 +0700

    UI for view list patient and handle for render view list patients

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,78 +1,122 @@
-import React from 'react'
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
+import React from "react";
 import axios from "axios";
 import { format } from "date-fns";
-import { get } from "https";
+// import { get } from "https";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { confirmAlert } from "react-confirm-alert";
 import "react-confirm-alert/src/react-confirm-alert.css";
-import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
+import { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TablePagination,
+  Button,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
 function Patients() {
+  const [patients, setPatients] = useState([]);
+  const [page, setPage] = useState(0); // trạng thái trang hiện tại
+  const [rowsPerPage, setRowsPerPage] = useState(10); // số hàng mỗi trang
+
+  useEffect(() => {
+    axios
+      .get(`http://127.0.0.1:8000/api/admin/patients`)
+      .then((res) => {
+        console.log(res);
+        setPatients(res.data.payload);
+      })
+      .catch((err) => {
+        console.error(err); // Để in ra lỗi nếu có
+      });
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  var patientDetails = "";
+  patientDetails = patients
+    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) // phân trang
+    .map((patient) => {
+      return (
+        <TableRow key={patient.id}>
+          <TableCell align="left">{patient.fullName}</TableCell>
+          <TableCell align="left">{patient.email}</TableCell>
+          <TableCell align="left">{patient.phone}</TableCell>
+          <TableCell align="left">{patient.address}</TableCell>
+          <TableCell align="left">{patient.healthCondition}</TableCell>
+          <TableCell align="left">{patient.note}</TableCell>
+          <TableCell align="right">
+            <div className="flex justify-center">
+              <Link to="/">EDIT</Link>
+              <Link to="/">DELETE</Link>
+            </div>
+          </TableCell>
+        </TableRow>
+      );
+    });
+
   return (
     <div>
       <div className="flex justify-between">
-          <h1 className="font-bold text-3xl mb-4">Management patients</h1>
-          <Button
-            
-            startIcon={<AddIcon />}
-          >
-            Add patient
-          </Button>
-        </div>
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
-          <TableContainer sx={{ maxHeight: 440 }}>
-            <Table stickyHeader aria-label="sticky table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left" style={{ minWidth: 70 }}>
-                    Name
-                  </TableCell>
-
-                  <TableCell align="left" style={{ minWidth: 100 }}>
-                    Email
-                  </TableCell>
-
-                  <TableCell align="left" style={{ minWidth: 70 }}>
-                    Phone
-                  </TableCell>
-
-                  <TableCell align="left" style={{ minWidth: 50 }}>
-                    Address
-                  </TableCell>
-
-                  <TableCell align="left" style={{ minWidth: 170 }}>
-                    Health Condition
-                  </TableCell>
-                  <TableCell align="left" style={{ minWidth: 170 }}>
-                    Note
-                  </TableCell>
-                  <TableCell align="center" style={{ minWidth: 170 }}>
-                    Action
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-
-              
-            </Table>
-          </TableContainer>
-          <TablePagination
-            
-          />
-        </Paper>
+        <h1 className="font-bold text-3xl mb-4">Management patients</h1>
+        <Button variant="contained">Add patient</Button>
+      </div>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left" style={{ minWidth: 70 }}>
+                  Name
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: 100 }}>
+                  Email
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: 70 }}>
+                  Phone
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: 50 }}>
+                  Address
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: 170 }}>
+                  Health Condition
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: 170 }}>
+                  Note
+                </TableCell>
+                <TableCell align="center" style={{ minWidth: 170 }}>
+                  Action
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{patientDetails}</TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={patients.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
     </div>
-  )
+  );
 }
 
-export default Patients
-
+export default Patients;
