acf379ef68af62f426939565dd45cc4f10822f91
luantranthannh <luan.tran25@student.passerellesnumeriques.org>
2024-06-03 23:33:16 +0700

    show chart to show the total doctors in each major

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,6 +14,7 @@ import CardComponent from "../components/CardComponent";
 import { Typography } from "@mui/material";
 import { Pie } from 'react-chartjs-2';
 import 'chart.js/auto';
+
 const DashboardContainer = styled("div")({
   flexGrow: 1,
   padding: 20,
@@ -53,7 +54,6 @@ export default function Dashboard() {
     fetchStats();
   }, []);
 
-
   useEffect(() => {
     const fetchDoctors = async () => {
       try {
@@ -71,7 +71,6 @@ export default function Dashboard() {
   }, []);
 
   const baseURL = "http://127.0.0.1:8000/images/";
-  
 
   useEffect(() => {
     const fetchMajors = async () => {
@@ -98,128 +97,138 @@ export default function Dashboard() {
     fetchMajors();
   }, []);
 
-
-
   return (
     <DashboardContainer>
       <Grid container spacing={3}>
-      <Grid item xs={12} sm={6} md={3}>
-        <CardComponent
-          title="TOTAL USERS"
-          value={stats.totalUsers}
-          iconType="money"
-          bgcolor="#009FFD"
-          isPositive={true}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <CardComponent
-          title="TOTAL PATIENTS"
-          value={stats.totalPatients}
-          iconType="users"
-          bgcolor="#FF416C"
-          isPositive={true}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <CardComponent
-          title="TOTAL DOCTORS"
-          value={stats.totalDoctors} 
-          iconType="clients"
-          bgcolor="#1FAF90"
-          isPositive={false}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <CardComponent
-          title="TOTAL REVENUE"
-          value={stats.totalRevenue + " VND"}
-          iconType="sales"
-          bgcolor="#FF7849"
-          isPositive={true}
-        />
-      </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <CardComponent
+            title="TOTAL USERS"
+            value={stats.totalUsers}
+            iconType="money"
+            bgcolor="#009FFD"
+            isPositive={true}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <CardComponent
+            title="TOTAL PATIENTS"
+            value={stats.totalPatients}
+            iconType="users"
+            bgcolor="#FF416C"
+            isPositive={true}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <CardComponent
+            title="TOTAL DOCTORS"
+            value={stats.totalDoctors}
+            iconType="clients"
+            bgcolor="#1FAF90"
+            isPositive={false}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <CardComponent
+            title="TOTAL REVENUE"
+            value={stats.totalRevenue + " VND"}
+            iconType="sales"
+            bgcolor="#FF7849"
+            isPositive={true}
+          />
+        </Grid>
       </Grid>
 
       <Grid container spacing={3}>
         <Grid item xs={12} md={8}>
-          <Typography variant="h5" sx={{ mt: 3, mb:2 }}>
+          <Typography variant="h5" sx={{ mt: 3, mb: 2 }}>
             Popular doctors
           </Typography>
           <Paper sx={{ mb: 3 }}>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center" style={{ minWidth: 200 }}>
-                  Name Doctor
-                </TableCell>
-
-                <TableCell align="center" style={{ minWidth: 150 }}>
-                  Email
-                </TableCell>
-
-                <TableCell align="center" style={{ minWidth: 120 }}>
-                  Major
-                </TableCell>
-                <TableCell align="center" style={{ minWidth: 70 }}>
-                  Slot
-                </TableCell>
-                <TableCell align="center" style={{ minWidth: 70 }}>
-                  Total money
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-            {doctors.map((doctor, index) => (
-            <TableRow key={index}>
-              <TableCell align="center">
-                <Avatar alt={doctor.doctorName}  src={`${baseURL}${doctor.avatar}`} />
-                {doctor.doctorName}
-              </TableCell>
-              <TableCell align="center">{doctor.doctorEmail}</TableCell>
-              <TableCell align="center">{doctor.doctorMajor}</TableCell>
-              <TableCell align="center">{doctor.totalSlots}</TableCell>
-              <TableCell align="center">{doctor.totalEarnings ? doctor.totalEarnings : '0.00'}</TableCell>
-            </TableRow>
-          ))}
-            </TableBody>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" style={{ minWidth: 200 }}>
+                    Name Doctor
+                  </TableCell>
+                  <TableCell align="center" style={{ minWidth: 150 }}>
+                    Email
+                  </TableCell>
+                  <TableCell align="center" style={{ minWidth: 120 }}>
+                    Major
+                  </TableCell>
+                  <TableCell align="center" style={{ minWidth: 70 }}>
+                    Slot
+                  </TableCell>
+                  <TableCell align="center" style={{ minWidth: 70 }}>
+                    Total money
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {doctors.length > 0 ? (
+                  doctors.map((doctor, index) => (
+                    <TableRow key={index}>
+                      <TableCell align="center">
+                        <Avatar alt={doctor.doctorName} src={`${baseURL}${doctor.avatar}`} />
+                        {doctor.doctorName}
+                      </TableCell>
+                      <TableCell align="center">{doctor.doctorEmail}</TableCell>
+                      <TableCell align="center">{doctor.doctorMajor}</TableCell>
+                      <TableCell align="center">{doctor.totalSlots}</TableCell>
+                      <TableCell align="center">{doctor.totalEarnings ? doctor.totalEarnings : '0.00'}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={5} align="center">
+                      No data available
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
           </Paper>
         </Grid>
 
         <Grid item xs={12} md={4}>
-        <Typography variant="h5" sx={{ mt: 3, mb:2 }}>
+          <Typography variant="h5" sx={{ mt: 3, mb: 2 }}>
             Popular majors
           </Typography>
-           <Paper sx={{ mb: 3 }}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell align="center" style={{ minWidth: 120 }}>
-              Name Doctor
-            </TableCell>
-            <TableCell align="center" style={{ minWidth: 150 }}>
-              Major
-            </TableCell>
-            <TableCell align="center" style={{ minWidth: 120 }}>
-              Pie Chart
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {majors.map((major, index) => (
-            <TableRow key={index}>
-              <TableCell align="center">{major.majorName}</TableCell>
-              <TableCell align="center">{major.totalDoctors}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      {chartData && (
-        <div style={{ width: '50%', margin: '0 auto' }}>
-          <Pie data={chartData} />
-        </div>
-      )}
-    </Paper>
-        
+          <Paper sx={{ mb: 3 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" style={{ minWidth: 120 }}>
+                    Major
+                  </TableCell>
+                  <TableCell align="center" style={{ minWidth: 150 }}>
+                    Total Doctors
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {majors.length > 0 ? (
+                  majors.map((major, index) => (
+                    <TableRow key={index}>
+                      <TableCell align="center">{major.majorName}</TableCell>
+                      <TableCell align="center">{major.totalDoctors}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={2} align="center">
+                      No data available
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+            {chartData && chartData.labels && chartData.labels.length > 0 && (
+              <div style={{ width: '70%', margin: '0 auto', padding: '20px' }}>
+                <Pie data={chartData} />
+              </div>
+            )}
+          </Paper>
         </Grid>
       </Grid>
     </DashboardContainer>
